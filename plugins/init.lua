bf1004b527967cb26b6399b6825c9d9394e2a926
Nang Chan <nangchan@gmail.com>
2023-10-24 19:54:44 -0700

    Remove unecessary plugins

--- plugins/init.lua
+++ plugins/init.lua
@@ -100,44 +100,6 @@ return {
     end,
   },
 
-  -- can diff file using :Gitsigns 
-  {
-    "lewis6991/gitsigns.nvim",
-    -- ft = { "gitcommit", "diff" },
-    -- init = function()
-    --   -- load gitsigns only when a git file is opened
-    --   vim.api.nvim_create_autocmd({ "BufRead" }, {
-    --     group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-    --     callback = function()
-    --       vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
-    --       if vim.v.shell_error == 0 then
-    --         vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
-    --         vim.schedule(function()
-    --           require("lazy").load { plugins = { "gitsigns.nvim" } }
-    --         end)
-    --       end
-    --     end,
-    --   })
-    -- end,
-    -- opts = function()
-    --   return require("custom.plugins.configs.others").gitsigns
-    -- end,
-    -- config = function(_, opts)
-    --   require("gitsigns").setup(opts)
-    -- end,
-  },
-
-  -- @todo figure out if useful when integrating lsp
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   init = function()
-  --     require("custom.core.utils").lazy_load "nvim-lspconfig"
-  --   end,
-  --   -- config = function()
-  --   --   require "custom.plugins.configs.lspconfig"
-  --   -- end,
-  -- },
-
   -- open terminal
   {
     "NvChad/nvterm",
